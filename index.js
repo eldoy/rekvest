a42745868ee4feef078f83c9d4d091e822e384ff
Vidar Eldøy <vidar@eldoy.com>
2021-03-09 19:43:36 +0100

    Decode href and pathname

--- index.js
+++ index.js
@@ -6,11 +6,13 @@ module.exports = function(req) {
   const proto = req.headers['x-forwarded-proto'] || (req.socket.encrypted ? 'https' : 'http')
 
   const url = new URL(`${proto}://${host}${req.url}`)
-
   for (const key in url) {
     req[key] = url[key]
   }
-  req.path = url.pathname + url.search
-  req.query = url.search ? qs.parse(url.search.slice(1)) : {}
+
+  req.href = decodeURI(req.href)
+  req.pathname = decodeURIComponent(req.pathname)
+  req.path = req.pathname + req.search
+  req.query = req.search ? qs.parse(req.search.slice(1)) : {}
   req.ip = req.socket.remoteAddress
 }
--- test/rekvest.test.js
+++ test/rekvest.test.js
@@ -31,4 +31,34 @@ describe('rekvest', () => {
     expect(req.query.hello).toBe('1')
     expect(req.path).toBe('/?hello=1')
   })
+
+  it('should parse requests urls with special characters', () => {
+    const req = {
+      url: '/æøå.html',
+      headers: {
+        host: 'localhost:3000'
+      },
+      socket: {}
+    }
+    rekvest(req)
+    expect(req.protocol).toBe('http:')
+    expect(req.port).toBe('3000')
+    expect(req.pathname).toBe('/æøå.html')
+    expect(req.path).toBe('/æøå.html')
+  })
+
+  it('should parse requests urls with chinese characters', () => {
+    const req = {
+      url: '/測試.html',
+      headers: {
+        host: 'localhost:3000'
+      },
+      socket: {}
+    }
+    rekvest(req)
+    expect(req.protocol).toBe('http:')
+    expect(req.port).toBe('3000')
+    expect(req.pathname).toBe('/測試.html')
+    expect(req.path).toBe('/測試.html')
+  })
 })
